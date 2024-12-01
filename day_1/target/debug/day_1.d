f1709fef3ee43834cf7bb13e44b0cfa3342e24cc
Spencer Scott <spencerscott917@gmail.com>
2024-12-01 11:14:57 -0700

    remove target

--- day_1/target/debug/day_1.d
+++
@@ -1,1 +0,0 @@
-/Users/gaia/repos/aoc_2024/day_1/target/debug/day_1: /Users/gaia/repos/aoc_2024/day_1/src/main.rs
--- day_1/target/debug/deps/day_1-c7ea2cab5e339170.d
+++
@@ -1,5 +0,0 @@
-/Users/gaia/repos/aoc_2024/day_1/target/debug/deps/libday_1-c7ea2cab5e339170.rmeta: src/main.rs
-
-/Users/gaia/repos/aoc_2024/day_1/target/debug/deps/day_1-c7ea2cab5e339170.d: src/main.rs
-
-src/main.rs:
