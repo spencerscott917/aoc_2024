15e6e67b8f47001ddd35a2161550cbaaf5929e8f
Spencer Scott <spencerscott917@gmail.com>
2024-12-01 11:15:59 -0700

    refactor main into multiple funcs so parts can be in same file

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -2,26 +2,36 @@ use std::fs::File;
 use std::io::prelude::*;
 
 fn main() -> std::io::Result<()> {
-    let mut f = File::open("input/input.txt")?;
+    let (mut col1, mut col2) = parse_input();
+    part1(&mut col1, &mut col2);
+    Ok(())
+}
+
+fn parse_input() -> (Vec<i32>, Vec<i32>) {
+    let mut f = File::open("input/input.txt").unwrap();
     let mut contents = String::new();
-    f.read_to_string(&mut contents)?;
+    f.read_to_string(&mut contents).unwrap();
     let mut col1 = Vec::new();
     let mut col2 = Vec::new();
     for l in contents.lines() {
         let mut columns = l.split("   ");
         if let Some(col) = columns.next() {
-            col1.push(col.parse::<i64>().unwrap());
+            col1.push(col.parse::<i32>().unwrap());
         }
         if let Some(col) = columns.next() {
-            col2.push(col.parse::<i64>().unwrap());
+            col2.push(col.parse::<i32>().unwrap());
         }
     }
-    let mut s: i64 = 0;
+    (col1, col2)
+}
+
+fn part1(col1: &mut Vec<i32>, col2: &mut Vec<i32>) {
+    let mut s: i32 = 0;
     col1.sort();
     col2.sort();
     for (x, y) in col1.iter().zip(col2.iter()) {
         s += (x - y).abs();
     }
-    println!("{s}");
-    Ok(())
+    println!("Part 1: {s}");
 }
+
