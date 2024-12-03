77d4aef0ef44c18781aa85a08e4ecb5a0b5fdeae
Spencer Scott <spencerscott917@gmail.com>
2024-12-03 09:06:34 -0700

    split row validity check into its own function

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -6,50 +6,45 @@ fn main() {
     let mut f = File::open("input/input.txt").unwrap();
     let mut contents = String::new();
     f.read_to_string(&mut contents).unwrap();
-    part_1(contents.clone());
+    let levels = contents.lines()
+                         .map(|line| {line.split_whitespace()
+                                         .map(|val| val.parse::<i32>().unwrap())
+                                         .collect::<Vec<i32>>()})
+                         .collect::<Vec<Vec<i32>>>();
+    part_1(&levels);
     part_2(contents.clone());
 }
 
-fn part_1(contents: String) {
+fn part_1(levels: &Vec<Vec<i32>>) {
     let mut num_safe = 0;
-    for line in contents.lines() {
-        let mut levels = Vec::new();
-        let level_is_valid: bool;
-        for level in line.split(" ") {
-            levels.push(level.parse::<i32>().unwrap());
-        }
-        match (levels[0] - levels[1]).cmp(&0) {
-             Ordering::Less => {
-                 level_is_valid = levels.iter()
-                                           .zip(levels.iter().skip(1))
-                                           .all(|(current, next)| ((current - next) <= -1) && ((current - next) >= -3));
-                 if level_is_valid {
-                     num_safe += 1;
-                 }
-             },
-             Ordering::Greater => {     
-                 level_is_valid = levels.iter()
-                                        .zip(levels.iter().skip(1))
-                                        .all(|(current, next)| ((current - next) >= 1) && ((current - next)) <= 3);
-                 if level_is_valid {
-                     num_safe += 1;
-                 }
-
-    },
-             _ => {num_safe += 0},
+    for row in levels {
+        if row_is_valid(&row) {
+            num_safe += 1
         }
     }
     println!("{num_safe}")
 }
 
-fn part_2(contents: String) {
+fn row_is_valid(row: &Vec<i32>) -> bool {
+    match (row[0] - row[1]).cmp(&0) {
+        Ordering::Less => {
+            row.iter()
+               .zip(row.iter().skip(1))
+               .all(|(current, next)| ((current - next) <= -1) && ((current - next) >= -3))
+        },
+        Ordering::Greater => {     
+            row.iter()
+               .zip(row.iter().skip(1))
+               .all(|(current, next)| ((current - next) >= 1) && ((current - next)) <= 3)
+        },
+        _ => false,
+    }
+}
+
+fn part_2(levels: &Vec<Vec<i32>>) {
     let mut num_safe = 0;
-    for line in contents.lines() {
-        let mut levels = Vec::new();
-        let level_is_valid: bool;
-        for level in line.split(" ") {
-            levels.push(level.parse::<i32>().unwrap());
-        }
+    for row in levels {
+
     }
 
     println!("{num_safe}");
