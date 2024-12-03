d4910dac51a261f4c88261fed0b162f0bdc8d70a
Spencer Scott <spencerscott917@gmail.com>
2024-12-03 10:12:44 -0700

    parse all valid mul patterns

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -1,3 +1,13 @@
+use std::{fs::File, io::prelude::*};
+use regex::Regex;
+
 fn main() {
-    println!("Hello, world!");
+    let mut file = File::open("input/input.txt").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("Could not read file contents to string");
+    let re = Regex::new(r"mul\([0-9][0-9]?[0-9]?,[0-9][0-9]?[0-9]?\)").unwrap();
+    for mat in re.find_iter(&contents) {
+            println!("-> {}", mat.as_str());
+    }
 }
