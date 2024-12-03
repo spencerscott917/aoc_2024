6af1958c434892d83d1675571ca20aa4cdf76ec9
Spencer Scott <spencerscott917@gmail.com>
2024-12-03 10:33:43 -0700

    solved day 3 part 1

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -6,8 +6,14 @@ fn main() {
     let mut contents = String::new();
     file.read_to_string(&mut contents)
         .expect("Could not read file contents to string");
-    let re = Regex::new(r"mul\([0-9][0-9]?[0-9]?,[0-9][0-9]?[0-9]?\)").unwrap();
-    for mat in re.find_iter(&contents) {
-            println!("-> {}", mat.as_str());
+    let re1 = Regex::new(r"mul\([0-9][0-9]?[0-9]?,[0-9][0-9]?[0-9]?\)").unwrap();
+    let re2 = Regex::new(r"[0-9][0-9]?[0-9]?,[0-9][0-9]?[0-9]?").unwrap();
+    let mut sum = 0;
+    for mat1 in re1.find_iter(&contents) {
+        for mat2 in re2.find_iter(mat1.as_str()) {
+            let vals: Vec<i32> = mat2.as_str().split(",").map(|x| x.parse::<i32>().unwrap()).collect();
+            sum += vals[0] * vals[1];
+        }
     }
+    println!("{sum}")
 }
